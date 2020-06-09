38716d43b9d6555b5d1f9dacd4910c9bf63d2386
Zen Wolf <zenisbestwolf@gmail.com>
2020-06-08 20:12:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# zen-minecraft-spigot-plugin-archetype
+A useful archetype with a godawful name.
---
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,9 @@
+package ${groupId}.${artifactId};
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	public void onEnable() {
+		// TODO: Fill in.
+	}
+}
